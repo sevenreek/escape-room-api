43fccea9d8283e53765b772032106498d1b6aa72
sevenreek <w.walewski112@gmail.com>
2021-09-24 12:22:04 +0200

    fixes mqtt messages by flushing

--- ctrl/main.py
+++ ctrl/main.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-
+import sys
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -10,15 +10,19 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    sys.stdout.flush()
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
-client.connect("localhost", 1883, 60)
-print("What?")
+print("Connecting mqtt...")
+sys.stdout.flush()
+client.connect("mqtt", 1883, 60)
+print("Connected")
+sys.stdout.flush()
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
+
 client.loop_forever()
